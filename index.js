7ba568510ddb009d8f04ff27aff7a43519a8e118
Sourav Haldar <souravhaldar2202@gmail.com>
2025-06-17 08:36:42 +0530

    before deployment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-    origin: 'https://lebaba-frontend-final-nu.vercel.app',
+    origin: 'http://localhost:5173',
     credentials: true
 }))
 
